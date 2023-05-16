572ce65394b99bb6a7c6996860b0dbd8ca14b2fe
unknown <bwheelz360@gmail.com>
2023-05-17 00:43:58 +1000

    add run_all script

---
+++ run_all.py
@@ -0,0 +1,10 @@
+import _0_data_location
+import _1_marker_extraction
+import _2_marker_matching
+import _3_field_calculation
+import _4_fit_harmonics
+import _4b_combine_harmonics
+import _5_reporting
+import _6_distortion_correction
+import _7_report_pre_correction
+import _7b_report_post_correction
